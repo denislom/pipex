f69dd8f44196f4aa4c1cccc435a33f049d323f9c
Denis Lom <lom.denis@seznam.cz>
2023-09-03 21:55:25 +0200

    loading 11.1/100

--- pipex.h
+++ pipex.h
@@ -6,7 +6,7 @@
 /*   By: dlom <dlom@student.42prague.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/02 16:15:18 by dlom              #+#    #+#             */
-/*   Updated: 2023/09/03 18:18:25 by dlom             ###   ########.fr       */
+/*   Updated: 2023/09/03 21:54:54 by dlom             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,10 @@
 # include <unistd.h>
 # include <fcntl.h>
 # include <stdlib.h>
+
+char	**ft_split(char const *s, char c);
+char	*ft_strdup(char *src);
+int	ft_strlen(const char	*str);
+int	ft_strncmp(const char *s1, const char *s2, size_t n);
 
 #endif
